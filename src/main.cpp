ef40b617b84a81dc4eaf93e5470aa485db866682
Anatoly Rybchych <tol.ryb@gmail.com>
2023-10-07 19:15:47 +0300

    Use effective type for Node indexes

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,22 @@
 #include <vector>
 
 template <typename IntergralOrEnumT>
-concept IntergralOrEnumType = std::integral<IntergralOrEnumT>
+concept IntergralOrEnumType = std::unsigned_integral<IntergralOrEnumT>
     || std::is_enum<IntergralOrEnumT>::value;
 
-template <typename UnitT>
-concept UnitType = requires(){
-    std::same_as<decltype(UnitT::COUNT), size_t>;
-    UnitT::COUNT != 0;
-
-    IntergralOrEnumType<typename UnitT::Type>;
-
-    std::same_as<decltype(UnitT::get), size_t (typename UnitT::Type)>;
-};
-
-template <typename NodeT>
-concept NodeType = requires(NodeT unit){
-    std::same_as<decltype(NodeT::COUNT), size_t>;
-    NodeT::COUNT != 0;
-
-    
+template<size_t SIZE>
+struct MinUint {
+    typedef 
+    typename std::conditional<(SIZE <= UINT8_MAX), 
+        uint8_t,
+        typename std::conditional<(SIZE <= UINT16_MAX), 
+            uint16_t,
+            typename std::conditional<(SIZE <= UINT32_MAX), 
+                uint32_t,
+                uint64_t
+            >::type
+        >::type
+    >::type Type;
 };
 
 template <typename TraveralT>
@@ -30,13 +27,11 @@ concept TraversalType = requires(TraveralT trait){
     IntergralOrEnumType<typename TraveralT::Unit>;
     IntergralOrEnumType<typename TraveralT::Node>;
 
-    std::same_as<decltype(TraveralT::unit_index), size_t(typename TraveralT::Unit)>;
+    std::same_as<decltype(TraveralT::unit_index), size_t(const typename TraveralT::Unit&)>;
     std::same_as<decltype(TraveralT::node_index), size_t(typename TraveralT::Node)>;
-    std::same_as<decltype(TraveralT::index_unit), typename TraveralT::Unit(size_t)>;
-    std::same_as<decltype(TraveralT::index_node), typename TraveralT::Node(size_t)>;
 
-    std::same_as<decltype(TraveralT::NODES_COUNT), size_t>;
-    std::same_as<decltype(TraveralT::UNITS_COUNT), size_t>;
+    std::unsigned_integral<decltype(TraveralT::NODES_COUNT)>;
+    std::unsigned_integral<decltype(TraveralT::UNITS_COUNT)>;
     TraveralT::NODES_COUNT != 0;
     TraveralT::UNITS_COUNT != 0;
 };
@@ -49,26 +44,26 @@ class Traversal{
     static constexpr size_t NODES_COUNT = TraversalT::NODES_COUNT;
     static constexpr size_t UNITS_COUNT = TraversalT::UNITS_COUNT;
 
-    static inline size_t unit_index(Unit unit){ return TraversalT::unit_index(unit); }
-    static inline Unit index_unit(size_t index){ return TraversalT::index_unit(index); }
+    static inline size_t unit_index(const Unit &unit){ return TraversalT::unit_index(unit); }
     static inline size_t node_index(Node node){ return TraversalT::node_index(node); }
-    static inline Node index_node(size_t index){ return TraversalT::index_node(index); }
-
 public:
     constexpr Traversal(){
-        for(size_t node_idx = 0; node_idx < NODES_COUNT; node_idx++){
-            Node node = index_node(node_idx);
-            set_all_branches(node, node);
+        for(size_t node_idx = 0; node_idx != NODES_COUNT; node_idx++){
+            for(size_t condition = 0; condition != UNITS_COUNT; condition++){
+                nodes[node_idx][condition] = node_idx;
+            }
         }
     }
 
-    constexpr void set_branch(Node from, Node to, Unit condition){
-        nodes[node_index(from)][unit_index(condition)] = to;
+    constexpr void set_branch(Node from, Node to, const Unit &condition){
+        _set_branch(node_index(from), node_index(to), unit_index(condition));
     }
 
     constexpr void set_all_branches(Node from, Node to){
+        size_t idx_from = node_index(from);
+        size_t idx_to = node_index(to);
         for(size_t condition = 0; condition < UNITS_COUNT; condition++){
-            set_branch(from, to, index_unit(condition));
+            _set_branch(idx_from, idx_to, condition);
         }
     }
 
@@ -76,9 +71,12 @@ public:
     constexpr void set_branch_if(Node from, Node to){
         ConditionChecker checker;
 
+        size_t idx_from = node_index(from);
+        size_t idx_to = node_index(to);
+
         for(size_t condition = 0; condition < UNITS_COUNT; condition ++){
             if(checker(condition)){
-                set_branch(from, to, index_unit(condition));
+                _set_branch(idx_from, idx_to, condition);
             }
         }
     }
@@ -86,9 +84,12 @@ public:
     template <typename Iter, typename Predicate>
     void traverse(Iter beg, Iter end, Node start_node, Predicate predicate){
         Iter chunk_beg = beg;
+
         Node cur_node = start_node;
+        size_t cur_node_idx = node_index(cur_node);
+
         for(Iter it = beg; it != end; it++){
-            Node node = nodes[node_index(cur_node)][unit_index(*it)];
+            Node node = nodes[cur_node_idx][unit_index(*it)];
             if(node != cur_node){
                 if(chunk_beg != it){
                     if(!predicate(cur_node, chunk_beg, it)){
@@ -96,40 +97,42 @@ public:
                     }
                     chunk_beg = it;
                 }
+
                 cur_node = node;
+                cur_node_idx = node_index(node);
             }
         }
     }
 private:
-    std::array<std::array<Node, UNITS_COUNT>, NODES_COUNT> nodes;
+    constexpr void _set_branch(size_t from, size_t to, size_t condition){
+        nodes[from][condition] = to;
+    }
+
+    using NodeIndex = MinUint<NODES_COUNT-1>::Type;
+    std::array<std::array<NodeIndex, UNITS_COUNT>, NODES_COUNT> nodes;
 };
 
 
 struct SplitTraversal{
-    enum Node {
+    enum NodeT {
         SPACE,
         NOT_SPACE,
 
-        NODES_COUNT,
+        __NODES_COUNT,
     };
 
+
+    using Node = unsigned char;
     using Unit = unsigned char;
     static constexpr size_t UNITS_COUNT = 256;
+    static constexpr size_t NODES_COUNT = __NODES_COUNT;
     
-    static constexpr size_t unit_index(Unit unit){
+    static constexpr size_t unit_index(const Unit &unit){
         return unit;
     }
 
     static constexpr size_t node_index(Node node){
         return node;
-    }
-
-    static constexpr Unit index_unit(size_t index){
-        return index;
-    }
-
-    static constexpr Node index_node(size_t index){
-        return (Node)index;
     }
 };
 
